4d9144dafd687114e9c863c68eda4c6f5a09f985
tkyg <80114433+tkyg@users.noreply.github.com>
2023-05-17 19:03:36 -0400

    creates and links components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import Experience from './components/Experience';
+import About from './components/About';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <div className="App">
       <Router>
         <NavBar />
         <Routes>
-          <Route>
-
-          </Route>
+          <Route exact path="/" element={ <Home /> }/>
+          <Route exact path="/projects" element={ <Projects /> }/>
+          <Route exact path="/experiences" element={ <Experience /> }/>
+          <Route exact path="/about" element={ <About /> }/>
         </Routes>
+        <Footer />
       </Router>
       
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div>
+      <div><button></button></div>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/experience">Experience</Link>
+        <Link to="/about">About</Link>
+      </div>
+    </div>
   )
 }
 
