03d5ae35952ca62693bba19a1814aae9caab3853
tkyg <80114433+tkyg@users.noreply.github.com>
2023-05-24 08:51:40 -0400

    adds project list

---
+++ src/components/pages/ProjectList.jsx
@@ -0,0 +1,48 @@
+import Phase1 from "../../images/Phase1.png"
+import Phase2 from "../../images/Phase2.png"
+import Phase3 from "../../images/Phase3.png"
+import Phase4 from "../../images/Phase4.png"
+import Capstone from "../../images/Capstone.png"
+
+export const ProjectList = [
+  {
+    name: "COCKTAIL RECIPES",
+    image: Phase1, 
+    description: "For my first project at Flatiron, I created a Single Page Application using the powerful trio of HTML, CSS, and JavaScript. I integrated the power of these three technologies to communicate with a public API, thus creating a dynamic and data-driven application. It was a rewarding endeavor providing me a hands-on experience with key web development concepts and tools.",
+    skills: "JavaScript, HTML, CSS",
+    github: "https://github.com/tkyg/CocktailProject.git",
+    link: "https://github.com/tkyg/CocktailProject.git"
+  },
+  {
+    name: "MY CUISINE",
+    image: Phase2, 
+    description: "In my React project, I delved into the world of component-based development, leveraging the capabilities of React Router to implement client-side router that facilitated seamless navigation. It was an immersive journey into the power and flexibility of React.",
+    skills: "JavaScript, React, HTML, CSS, StyledComponents",
+    github: "https://github.com/tkyg/my-cuisine.git",
+    link: "https://mycuisine.herokuapp.com/"
+  },
+  {
+    name: "MY RESERVATION",
+    image: Phase3, 
+    description: "The objective of this project was to construct a backend using Sinatra and Active Record, which functioned as an API. To facilitate database interactions, I developed a separate React frontend. I gained valuable insights into the importance of separating concerns and understanding the communication process between frontend and backend components.",
+    skills: "JavaScript, React, Ruby, Sinatra, Active Record, HTML, CSS",
+    github: "https://github.com/tkyg/my-reservation-project.git",
+    link: "https://my-reservation-project.herokuapp.com/"
+  },
+  {
+    name: "BLOGGER'S DIGEST",
+    image: Phase4,
+    description: "I developed a full-stack project with a React frontend and a Rails backend. Implementing authentication and authorization, including password protection. I gained valuable experience in creating secure web applications that prioritize user privacy and data protection.", 
+    skills: "JavaScript, React, Ruby, Ruby on Rails, Bcrypt, HTML, CSS",
+    github: "https://github.com/tkyg/bloggers_digest.git",
+    link: "https://bloggers-digest.onrender.com"
+  },
+  {
+    name: "GRUBGO",
+    image: Capstone, 
+    description: "During my capstone project, I envisioned something more than just a final assignment. It became a personal endeavor that I wanted to continue even after completing my journey at Flatiron School. To make it happen, I divided the project into three phases. I have successfully completed Phase 1, and now I'm eagerly looking forward to the next two phases. This project holds a special place in my heart as it symbolizes the culmination of my time at Flatiron and serves as a constant reminder of my passion for growth and development. It's a story that continues to unfold.",
+    skills: "JavaScript, React, Redux, Thunk, Ruby, Ruby on Rails, Bcrypt, HTML, CSS",
+    github: "https://github.com/tkyg/GrubGo.git",
+    link: "https://github.com/tkyg/GrubGo.git"
+  },
+]
