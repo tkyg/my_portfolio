207907e83073330abdac48a1a8a7c4a68faf7964
tkyg <80114433+tkyg@users.noreply.github.com>
2023-05-24 08:58:54 -0400

    renders project card

--- src/components/pages/ProjectCard.jsx
+++ src/components/pages/ProjectCard.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import '../styles/projectcard.css'
 
-const ProjectCard = () => {
+const ProjectCard = (props) => {
+
+  const handleGitHubClick = () => {
+    window.open(props.github, '_blank')
+  }
+  const handleLinkClick = () => {
+    window.open(props.link, '_blank')
+  }
+
   return (
-    <div>ProjectCard</div>
+    <div className='project'>
+      <h1>{props.name}</h1>
+      <p>{props.description}</p>
+      <br />
+      <p className='project-skill'>SKILLS: {props.skills}</p>
+      <br/>
+      <div>
+        <button onClick={handleGitHubClick}>GitHub</button>
+        <button onClick={handleLinkClick}>{props.name}</button>
+      </div>
+      <div className='project-image'>
+        <img src={props.image} alt={props.name}/>
+      </div>
+    </div>
+    
   )
 }
 
