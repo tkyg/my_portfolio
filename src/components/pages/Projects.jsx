b4017045a8246a2d24c2220e90f24b2e62e27efb
tkyg <80114433+tkyg@users.noreply.github.com>
2023-05-24 08:56:14 -0400

    builds projects and project card components

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import ProjectCard from './ProjectCard'
+import { ProjectList } from './ProjectList'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='projects'>
+      {/* <h1>My Projects</h1> */}
+      <div>
+        {ProjectList.map((project, index) => {
+          return (
+            <ProjectCard 
+              key={index} 
+              name={project.name} 
+              image={project.image} 
+              description={project.description} 
+              skills={project.skills}
+              github={project.github}
+              link={project.link}
+            />
+          )
+        })}
+      </div>
+    </div>
   )
 }
-
 export default Projects
